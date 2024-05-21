06f10ca12052ac36944d6006ab2ca58fd79901db
Ruslan <almakaev-2011@mail.ru>
2024-05-21 17:43:21 +0500

    update

--- app/db/wetherModel.py
+++ app/db/wetherModel.py
@@ -4,17 +4,17 @@ import databases
 from sqlalchemy.ext.declarative import declarative_base
 
 from ..db.config import settings
-# from .BaseMeta import BaseMeta, metadata
-DATABASE_URL = "postgresql://fastapi_traefik:fastapi_traefik@db:5432/fastapi_traefik"
 
 database = databases.Database(settings.db_url)
 metadata = sqlalchemy.MetaData()
 Base = declarative_base()
 
+
 class BaseMeta(ormar.ModelMeta):
     metadata = metadata
     database = database
-    
+
+
 class Weather(ormar.Model):
     class Meta(BaseMeta):
         tablename = "weather"
@@ -24,8 +24,4 @@ class Weather(ormar.Model):
     temperature: int = ormar.Integer(nullable=False)
 
 engine = sqlalchemy.create_engine(settings.db_url)
-# metadata.drop_all(engine)
 metadata.create_all(engine)
-# Base.metadata.create_all(engine)
-# Weather = Weather(date="2018-01-01", temperature=1, completed=False)
-# Weather.save()
